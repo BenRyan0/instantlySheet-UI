4f4ff6cdd26945742a7523cb64779bd2a9f464c2
BenRyan0 <benryanrinconada5@gmail.com>
2025-11-11 01:32:34 +0800

    SavePoint -copiesKept_1 -benRyan

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,7 +5,7 @@ import { LoginForm } from './../components/login-form';
 const Login = () => {
   return (
     <div className="relative h-screen w-full p-0 flex justify-center items-center text-white">
-        <LoginForm className="lg:w-4/12 md:w-6/12 w-9/12"/>
+        <LoginForm className="lg:w-3/12 md:w-6/12 w-9/12"/>
     </div>
   )
 }
